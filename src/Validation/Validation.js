065f9d63a068072aee7f125c091443d923d96984
Javier Rojas <jrojas03.08.98@gmail.com>
2020-03-17 15:10:33 -0400

    better implementations on validation

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const ValidationComponent = (props) => {
+const ValidationComponent = props => {
+  const { length } = props;
 
-    const {length} = props;
-    let text = 'Text too short'
-    if(length >= 5) text='Text long enough' 
+  //   const text = (() => {
+  //     if (length >= 5) {
+  //       return "Text long enough";
+  //     }
 
-    return(
-        <div>
-            <p>{text}</p>
-        </div>
-    );
+  //     return "Text too short";
+  //   })();
+  // In case more than 2 options its better this way ^^
 
-}
+  //   let text = "Text too short";
+  //   if (length >= 5) text = "Text long enough";
+  // not the best way of doing it ^^
+
+  return (
+    <div>
+      <p>{length >= 5 ? "Text long enough" : "Text too short"}</p>
+
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default ValidationComponent;
